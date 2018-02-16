944c9dd4d3ebcd0bc5d1bb7446d5b2b0c72b5363
kouta kon <sound.recording.man@gmail.com>
2018-02-16 10:44:04 +0900

    Sampleを修正

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -6,28 +6,52 @@ namespace Sample
     {
         static void Main(string[] args)
         {
+            string localName = args[0];
+
             BleLib.BleDevice device = new BleLib.BleDevice();
             device.BleEvent += Device_BleEvent;
 
             // Connect to Ble device.
-            bool result = device.Connect("Local Name", 5000);
+            bool result = device.Connect(localName, 10000);
             if (!result)
             {
                 Console.WriteLine("Failed to connection.");
                 return;
             }
 
-            // Get service uuid array.
-            Guid[] service = device.GetServiceUuids();
-            foreach (Guid s in service)
+            if (args.Length < 2)
             {
-                Console.WriteLine($"Service UUID        : {s}");
-
-                // Get characteristic uuid array.
-                Guid[] chara = device.GetCharacteristicUuids(s);
-                foreach (Guid c in chara)
+                // Get service uuid array.
+                Guid[] service = device.GetServiceUuids();
+                foreach (Guid s in service)
                 {
-                    Console.WriteLine($"Characteristic UUID : {c}");
+                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine($"Service UUID        : {s}");
+
+                    // Get characteristic uuid array.
+                    Guid[] chara = device.GetCharacteristicUuids(s);
+                    foreach (Guid c in chara)
+                    {
+                        Console.WriteLine($"Characteristic UUID : {c}");
+                        //device.Read(s, c);
+                    }
+                }
+            }
+            else
+            {
+                Guid s = Guid.Parse(args[1]);
+                Guid c = Guid.Parse(args[2]);
+
+                Console.WriteLine($"{s} {c}");
+
+                byte[] data = device.Read(s, c);
+                if (data != null)
+                {
+                    Console.WriteLine(BitConverter.ToString(data));
+                }
+                else
+                {
+                    Console.WriteLine("null");
                 }
             }
 
